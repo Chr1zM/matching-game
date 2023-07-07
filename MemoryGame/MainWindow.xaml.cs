33da7ca32bac334d8b4808490bd7a6e9210eed99
Chr1zM <ch.me010103@gmail.com>
2023-07-07 22:31:55 +0200

    * refactor

--- MemoryGame/MainWindow.xaml.cs
+++ MemoryGame/MainWindow.xaml.cs
@@ -15,15 +15,39 @@ using System.Windows.Shapes;
 
 namespace MemoryGame
 {
+    using System.Windows.Threading;
+
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int tenthsOfSecondsElapsed;
+        private int matchesFound;
+
+
+        private TextBlock lastTextBlockClicked;
+        private bool findingMatch = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Nochmal spielen?";
+            }
         }
 
         private void SetUpGame()
@@ -44,15 +68,18 @@ namespace MemoryGame
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
+                if (textBlock.Name == "timeTextBlock") continue;
+
                 int index = random.Next(animalList.Count);
                 string nextAnimal = animalList[index];
                 textBlock.Text = nextAnimal;
                 animalList.RemoveAt(index);
             }
-        }
 
-        TextBlock lastTextBlockClicked;
-        bool findingMatch = false;
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
 
         private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -65,6 +92,7 @@ namespace MemoryGame
             }
             else if (textBlock.Text == lastTextBlockClicked.Text)
             {
+                matchesFound++;
                 textBlock.Visibility = Visibility.Hidden;
                 findingMatch = false;
             }
@@ -74,7 +102,14 @@ namespace MemoryGame
                 findingMatch = false;
             }
         }
-    }
 
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
+        }
+    }
 
 }
