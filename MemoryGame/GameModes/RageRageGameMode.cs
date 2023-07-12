3573dd3e9bd8155f82a650a8860ac2e7f58c74f4
Chr1zM <ch.me010103@gmail.com>
2023-07-13 01:02:42 +0200

    * WIP Neue Spielmodi einbauen

---
+++ MemoryGame/GameModes/RageRageGameMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame.GameModes
+{
+    internal class RageRageGameMode
+    {
+        // TODO implement
+    }
+}
